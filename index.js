63246c17d2b034f428dc65f3a8b46548f0d0f8ae
Mulaa <louismulaa06@gmail.com>
2024-03-24 22:34:34 +0300

    objects lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+const employee= {
+    name: "Louis Mulaa",
+    streetAddress:"574 Ngong",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee,[key]: value };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+employee[key] = value;
+return employee;}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const 
+ return newEmployee;
+}
